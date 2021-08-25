c0fbd8730fbb05856eb650b6182c27efc4c6b9f7
edwardchen <edwardchen@123u.com>
2021-08-25 14:37:27 +0800

    feat(all): 结果写入csv文件，进度条，更新README说明

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # unity-yaml-searcher
 使用unity-yaml-parser进行项目资源检索
+
+### 运行环境
+
+括号内为开发环境
+
+- python3(3.8.5)
+- tpdm(4.51.0)
+- unityparser(2.0.0)
+
+### 使用说明
+
+导出结果为csv格式文件，其中：
+
+1. ResourceName：引用特效的资源名
+2. ResourcePath：引用特效资源的绝对路径
+3. Type：应用类型
+   1. effect helper：特效助手引用
+   2. prefab instance：直接放置特效prefab
+4. EffectName：特效名称
+5. EffectPath：特效相对Resources文件夹目录
--- search.py
+++ search.py
@@ -1,13 +1,45 @@
 from unityparser import UnityDocument
+from tqdm import tqdm
 import json
 import os
+import csv
+import time
 
 SYS_ENV_JSON_PATH = 'C:/ROGameFeature/sys_env.json'
+# 写入CSV文件
+output_file_path = 'result.csv'
 
-EFFECT_RESOURCE_PREFIX = 'fx'
+UI_EFFECT_PREFIX = 'fx_ui'
 PREFAB_FILE_SUFFIX = '.prefab'
 MOON_RES_PATH = 'MoonResPath'
 UI_PREFAB_RELATIVE_PATH = '/Resources/UI/Prefabs/'
+UI_EFFECT_RELATIVE_PATH = 'Effects/Prefabs/Creature/Ui/'
+
+# CSV文件表头
+headers = ['ResourceName', 'ResourcePath', 'Type', 'EffectName', 'EffectPath']
+# CSV文件内容
+rows = []
+
+def local_time_str(format:str=None):
+    """
+    日志用
+    :param format: 格式字符串，为空默认"%Y-%m-%d %H:%M:%S"
+    :return: 本地时间字符串
+    """
+    formatStr = format or '%Y-%m-%d %H:%M:%S'
+    return time.strftime(formatStr, time.localtime())
+
+
+def add_row_data(resource_name, resource_path, type, effect_name, effect_path):
+    """
+    往文件中写入一条记录
+    :param resource_name: 引用特效资源名
+    :param resource_path: 引用特效资源路径
+    :param type: 资源类型
+    :param effect_name: 特效名
+    :param effect_path: 特效路径
+    """
+    rows.append([resource_name, resource_path, type, effect_name, effect_path])
 
 def load_and_filter_yaml(file_path):
     """
@@ -15,20 +47,27 @@ def load_and_filter_yaml(file_path):
     :param file_path: yaml文件绝对路径
     """
     doc = UnityDocument.load_yaml(file_path=file_path)
+    file_name = file_path[file_path.rfind('/')+1:]
     # 特效助手配置路径
     entries = doc.filter(('MonoBehaviour',), ('EffectPath',))
     for entry in entries:
-        print(f"[{file_path}][EFFECT HELPER]: {entry.EffectPath}")
+        effect_path = entry.EffectPath
+        effect_name = effect_path[effect_path.rfind('/')+1:]
+        add_row_data(file_name, file_path, 'effect helper', effect_name, effect_path)
+        # print(f"[{file_path}][EFFECT HELPER]: {entry.EffectPath}")
 
     # 特效prefab直接放在UIprefab中
     entries = doc.filter(('PrefabInstance',))
     for entry in entries:
         modifications = entry.m_Modification['m_Modifications']
         for modification in modifications:
-            if modification['propertyPath'] == 'm_Name':
-                name = modification['value']
-                if str(name)[:2].lower() == EFFECT_RESOURCE_PREFIX:
-                    print(f"[{file_path}][PREFAB INSTANCE]: {name}")
+            if modification['propertyPath'] != 'm_Name':
+                continue
+            name = modification['value']
+            if str(name)[:5].lower() != UI_EFFECT_PREFIX:
+                continue
+            add_row_data(file_name, file_path, 'prefab instance', name, UI_EFFECT_RELATIVE_PATH + name)
+            # print(f"[{file_path}][PREFAB INSTANCE]: {name}")
 
 
 def walk_through_directory(directory):
@@ -37,6 +76,7 @@ def walk_through_directory(directory):
     :param directory: 根目录
     """
     prefab_path_list = []
+    print(f'[{local_time_str()}] 对\'{directory}\'目录深度搜索搜索...')
     for root, _, files in os.walk(directory):
         for file in files:
             file_path = os.path.join(root, file).replace('\\', '/')
@@ -44,24 +84,44 @@ def walk_through_directory(directory):
                 continue
             prefab_path_list.append(file_path)
     
-    for prefab_path in prefab_path_list:
+    pbar = tqdm(prefab_path_list)
+    for prefab_path in pbar:
         try:
             load_and_filter_yaml(prefab_path)
+            pbar.set_description_str(f'正在处理\'{prefab_path}\'')
         except Exception as e:
             print(f'[{prefab_path}] 解析YAML时发生错误')
             print(e)
 
 
+def save_result():
+    """
+    将搜索结果写入到`OUTPUT_FILE_PATH`
+    """
+    write_success = False
+    output_file_path = local_time_str('%Y%m%d%H%M%S') + '.csv'
+    with open(output_file_path, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+        writer.writerows(rows)
+        write_success = True
+    
+    result_str = write_success and '成功' or '失败'
+    print(f'[{local_time_str()}] 特效引用搜索结果输出到文件\'{output_file_path}\'{result_str}')
+
+
 def main():
+    print(f'[{local_time_str()}] 开始特效引用搜索...')
+
     fp = open(SYS_ENV_JSON_PATH, 'r')
     sys_env = json.load(fp)
     if MOON_RES_PATH in sys_env.keys():
         artres_path = sys_env[MOON_RES_PATH]
         walk_through_directory(artres_path + UI_PREFAB_RELATIVE_PATH)
+    save_result()
 
 
 if __name__ == '__main__':
-    # TODO 进度条
     # TODO 脚本参数传入
-    # TODO 结果写入文件
+    # TODO .unity特效查找
     main()
